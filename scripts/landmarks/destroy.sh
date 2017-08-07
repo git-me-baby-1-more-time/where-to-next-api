207380ee5fe8571687bee8eae73c7e1ecab6db13
rtuttle611 <rtuttle611@yahoo.com>
2017-08-07 13:46:42 -0400

    added curl script for remove landmark

--- scripts/landmarks/destroy.sh
+++ scripts/landmarks/destroy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/activities"
+URL_PATH="/landmarks"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
@@ -9,7 +9,7 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
-    "activity": {
+    "landmark": {
       "name": "'"${NAME}"'"
     }
   }'
